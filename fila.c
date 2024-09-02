109b52f22ca25257149b8fabc442930c2b073710
Andre Noel <andre@andrenoel.com.br>
2024-09-02 19:07:27 -0300

    Aquivo fila completo

--- fila.c
+++ fila.c
@@ -1,14 +1,15 @@
 #define TAMANHO 10
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct fila {
     int inicio;
     int fim;
-    int arrayFila[TAMANHO];
+    int dados[TAMANHO];
 } TFila;
 
-TFila fila;
+TFila fila; 
 
 int tamanhoDaFila() {
     if (fila.fim - fila.inicio < 0) {
@@ -18,40 +19,67 @@ int tamanhoDaFila() {
 }
 
 void insereNaFila(int el) {
-    // Testar:
-    // Só posso inserir se o tamanho da fila for menor que o tamanho do vetor
-
-    fila.arrayFila[fila.fim] = el;
+    if (tamanhoDaFila() >= TAMANHO) {
+        printf("Fila cheia!\n");
+        return;
+    }
+    
+    fila.dados[fila.fim] = el;
     fila.fim++;
 }
 
 int removeDaFila() {
-    // Testar:
-    // Só posso remover da fila se o tamanho da fila for maior que zero
-    int el = fila.arrayFila[fila.inicio];
+    if (tamanhoDaFila() <= 0) {
+        printf("Fila vazia!\n");
+        return -1;
+    }
+
+    int el = fila.dados[fila.inicio];
     fila.inicio++;
     return el;
 }
 
 void mostrarFila() {
     for (int i = fila.inicio; i < fila.fim; i++) {
-        printf("%d ", fila.arrayFila[i]);
+        printf("%d ", fila.dados[i]);
     }
     printf("\n");
 }
 
 
 int main() {
-    insereNaFila(8);
-    insereNaFila(3);
-    insereNaFila(4);
-    printf("Tamanho da fila: %d\n", tamanhoDaFila());
-    mostrarFila();
-    printf("Atendimento: %d\n", removeDaFila());
-    mostrarFila();
-    printf("Início da fila: %d\n", fila.inicio);
-    printf("Fim da fila: %d\n", fila.fim);
+    while (1 == 1) {
+        printf("--- BEM-VINDO AO SISTEMA SUPIMPA ---\n\n");
+        printf("O que você deseja fazer hoje?\n\n");
+        printf("1 - Exibir a fila\n");
+        printf("2 - Inserir na fila\n");
+        printf("3 - Remover da fila\n");
+        printf("0 - Sair\n\n: ");
+        int opcao, add;
+        scanf("%d", &opcao);
 
-    
+        switch (opcao) {
+            case 1:
+                mostrarFila();
+                system("pause");
+                break;
+            case 2:
+                printf("Escolha um número para adicionar: ");
+                scanf("%d", &add);
+                insereNaFila(add);
+                break;
+            case 3:
+                add = removeDaFila();
+                printf("%d saiu da fila\n", add);
+                system("pause");
+                break;
+            case 0:
+                return 0;
+            default:
+                break;
+        }
+
+    }
+
     return 0;
 }
